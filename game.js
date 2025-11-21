8937be8dc5259b86e2ab8b94e97eb16c04c4aee1
Inna Rego <inna.rego@ivkhk.ee>
2025-11-21 19:42:53 +0200

    Уничтожение блоков

--- game.js
+++ game.js
@@ -27,9 +27,10 @@ let game = {
     window.addEventListener("keydown", e => {
       if (e.keyCode === KEYS.SPACE) {
         this.platform.fire();
-      } else if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
-        this.platform.start(e.keyCode);
       }
+      else if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
+        this.platform.start(e.keyCode);
+      } 
     });
     window.addEventListener("keyup", e => {
       this.platform.stop();
@@ -43,7 +44,7 @@ let game = {
       if (loaded >= required) {
         callback();
       }
-      };
+    };
 
     for (let key in this.sprites) {
         this.sprites[key] = new Image();
@@ -55,6 +56,7 @@ let game = {
     for (let row = 0; row < this.rows; row++) {
       for (let col = 0; col < this.cols; col++) {
         this.blocks.push({
+          active: true,
           width: 60,
           height: 20,
           x: 64 * col + 65,
@@ -71,7 +73,7 @@ let game = {
   },
   collideBlocks() {
     for (let block of this.blocks) {
-      if (this.ball.collide(block)) {
+      if (block.active && this.ball.collide(block)) {
         this.ball.bumpBlock(block);
       }
     }
@@ -81,8 +83,6 @@ let game = {
       this.ball.bumpPlatform(this.platform);
     }
   },
-    
-  
   run() {
     window.requestAnimationFrame(() => {
         this.update();
@@ -90,7 +90,7 @@ let game = {
         this.run();
     });
   },
-   render() {
+  render() {
     this.ctx.clearRect(0, 0, this.width, this.height);
     this.ctx.drawImage(this.sprites.background, 0, 0);
     this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
@@ -99,7 +99,10 @@ let game = {
   },
   renderBlocks() {
     for (let block of this.blocks) {
-      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      if (block.active){
+        this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      }
+      
     }
   },
   start: function() {
@@ -113,11 +116,10 @@ let game = {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 };
-
 game.ball = {
     dx: 0,
     dy: 0,
-    velocity: 3,
+  velocity: 3,
     x: 320,
     y: 280,
     width: 20,
@@ -130,33 +132,33 @@ game.ball = {
       if (this.dy) {
         this.y += this.dy;
       }
-      if(this.dx) {
+      if (this.dx) {
         this.x += this.dx;
       }
-  },
-  collide(element) {
-    let x = this.x + this.dx;
-    let y = this.y + this.dy;
+    },
+    collide(element) {
+      let x = this.x + this.dx;
+      let y = this.y + this.dy;
 
-    if (x + this.width > element.x &&
-        x < element.x + element.width &&
-        y + this.height > element.y &&
-        y < element.y + element.height) {
-          return true;
-        } 
-        return false;
-      },
-      bumpBlock(block) {
-        this.dy *= -1;
-      },
-      bumpPlatform(platform) {
-        this.dy *= -1;
-        let touchX = this.x + this.width / 2;
-        this.dx = this.velocity * platform.getTouchOffset(touchX);
-      }
-      };
-
-   game.platform = { 
+      if (x + this.width > element.x &&
+          x < element.x + element.width &&
+          y + this.height > element.y &&
+          y < element .y + element.height) {
+            return true;
+          }
+      return false;
+    },
+    bumpBlock(block) {
+      this.dy *= -1;
+      block.active = false;
+    },
+    bumpPlatform(platform) {
+      this.dy *= -1;
+      let touchX = this.x + this.width / 2;
+      this.dx = this.velocity * platform.getTouchOffset(touchX);
+    }
+ };
+game.platform = {
     velocity: 6,
     dx: 0,
     x: 280,
@@ -184,17 +186,16 @@ game.ball = {
       if (this.dx) {
         this.x += this.dx;
         if (this.ball) {
-           this.ball.x += this.dx;
+          this.ball.x += this.dx;
         }
       }
-    
-},
-getTouchOffset(x) {
-  let diff = (this.x + this.width) - x;
-  let offset = this.width - diff;
-  let result = 2 * offset / this.width;
-  return result - 1;
-  }
+    },
+    getTouchOffset(x) {
+      let diff = (this.x + this.width) -x;
+      let offset = this.width - diff;
+      let result = 2 * offset / this.width;
+      return result - 1;
+    }
 };
 window.addEventListener("load", () => {
     game.start();
